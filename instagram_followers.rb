32a81c5ebd82a1be819a7f46e2fa21e0ad74842f
Llorenc Muntaner <llorenc.muntaner@gmail.com>
2014-10-26 19:43:39 -0700

    Changed the way to request every follower. Still creating one for each follower, I need to improve that.

--- instagram_followers.rb
+++ instagram_followers.rb
@@ -11,58 +11,36 @@ class InstagramBotSearcher
     end
     @username = username
     @id = get_id
-    @follower_ids = get_follower_ids
   end
   
   def access_token
     @access_tokens.first
   end
-    
+
   def check_followers
-    humans = []
-    bots = []
-    @follower_ids.each do |follower_id|
-      p follower_id
-      p is_bot?(follower_id)
-      if is_bot?(follower_id)
-        bots << follower_id
-      else
-        humans << follower_id
+    url = "https://api.instagram.com/v1/users/#{@id}/followed-by?#{access_token}"
+    valids, not_valids = 0, 0
+    while true
+      parsed_response = make_request(url)
+      parsed_response["data"].each do |follower|
+        is_valid?(follower["id"]) ? not_valids += 1 : valids += 1
       end
+      break if parsed_response["pagination"]["next_url"].nil?
+      url = parsed_response["pagination"]["next_url"]
     end
-    [humans.count, bots.count]
+    [valids, not_valids]
   end
   
   def get_id
     url = "https://api.instagram.com/v1/users/search?q=[#{@username}]&#{access_token}"
     parsed_response = make_request(url)
-    id = parsed_response["data"].first["id"]
-  end
-
-  def get_follower_ids
-    access_token = @access_tokens.first
-    url = "https://api.instagram.com/v1/users/#{@id}/followed-by?#{access_token}"
-    ids = []
-    while true
-      parsed_response = make_request(url)
-      break if parsed_response["pagination"]["next_url"].nil?
-      parsed_response["data"].each do |follower|
-        ids << follower["id"]
-      end
-      url = parsed_response["pagination"]["next_url"]
-      parsed_response = make_request(url)
-    end
-    ids
-  end
-
-  def is_bot?(id)
-    url = "https://api.instagram.com/v1/users/#{id}/?#{access_token}"
-    parsed_response = make_request(url)
-    return true unless parsed_response
-    num_following = parsed_response["data"]["counts"]["follows"]
-    num_following > 200
+    parsed_response["data"].first["id"]
   end
   
+  def is_valid?(id)
+    many_followings?(id)
+  end
+
   def make_request(url)
     response = RestClient.get(url) do |response, request, result| 
       return false if response.code == 400
@@ -71,6 +49,18 @@ class InstagramBotSearcher
     JSON.parse(response)
   end
   
+  def many_followings?(id)
+    num_following(id) > 400
+  end
+
+  def num_following(id)
+    url = "https://api.instagram.com/v1/users/#{id}/?#{access_token}"
+    puts "requesting..."
+    parsed_response = make_request(url)
+    return 0 unless parsed_response
+    parsed_response["data"]["counts"]["follows"]
+  end
+    
   def result
     pair_followers = check_followers
     puts "#{username} has #{pair_followers[0]} humans and #{pair_followers[1]} bots."
@@ -78,7 +68,7 @@ class InstagramBotSearcher
 end
 
 access_token = "8413639.1fb234f.c0f10cda7f6e4234bc23be65137d5826"
-p = InstagramBotSearcher.new("letisalabufill", access_token)
+p = InstagramBotSearcher.new("marinuxi", access_token)
 
 p.result
 
